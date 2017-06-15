b89d51f476fa1ec0c9aafdc913f2c1725a30f949
Alex Peng <jjpeng@ryerson.ca>
2017-06-15 11:32:07 -0400

    added proper save settings

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-A learning tool
+# Language Immersion Extension For Firefox
+----
+This firefox extension is able to help you learn assorted languages by replacing the text inside of the webpages with the equivalent word. Normal paragraphs will contain words of the language the user would like to learn. 
--- choose_lang.js
+++ choose_lang.js
@@ -1,11 +1,47 @@
-document.getElementById("save_settings").onclick = function(){
-	document.getElementById("update").innerHTML = "Settings updated";
-	//alert("hello world");
+function saveOptions(e){
+	e.preventDefault();
+	var is_enabled = document.getElementById('enable');
+	var translate_frequency = document.getElementById('translate_frequency').value;
+	//var translateText = document.getElementById('words').value;
+	if(document.getElementById('translate_frequency').value > 100){
+		translate_frequency = document.getElementById('translate_frequency').max;
+	}
+
+	var language_original = document.getElementById('original').value;
+	var language_translated = document.getElementById('translated').value;
+	var allow_hyperlink = document.getElementById('hyperlink').value;
+
+	document.getElementById("save_settings").onclick = function(){
+		document.getElementById("update").innerHTML = "Settings updated";
+	}
+	browser.storage.local.set({
+		save_is_enabled : is_enabled,
+		save_translate_frequency : translate_frequency,
+		save_language_original : language_original,
+		save_language_translated : language_translated,
+		save_allow_hyperlink : allow_hyperlink	
+	})
 }
 
+function restoreOptions() {
+
+  function setCurrentChoice(result) {
+    document.querySelector("#color").value = result.color || "blue";
+  }
+
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+
+  var getting = browser.storage.local.get("color");
+  getting.then(setCurrentChoice, onError);
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
 
 /*
-document.getElementsById("save").addEventListener("click", function(){
+document.getElementById("save").addEventListener("click", function(){
 	var paragraph = document.getElementByTagName("p");
 	var i;
 	for(i = 0 ; i < paragraph.length() ; i++){
